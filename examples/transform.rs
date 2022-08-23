3b4c97c596bf24a7a4bd86128c99d38f5300c089
Rose Hudson <cv21874@bristol.ac.uk>
2022-08-23 13:41:37 +0100

    add comment to transform example apologising for crimes against the type system

--- examples/transform.rs
+++ examples/transform.rs
@@ -8,6 +8,11 @@ use frunk::Coprod;
 
 fn main() {
     let work = take_over_the_world();
+    // At this point we are starting to hit the limits of what is reasonably possible without
+    // compiler support. The compiler has to infer a set of effects that is a superset of both the
+    // original effect set and the effect set of the handler, but since there are infinite
+    // possibilities when our only bounds are "is a superset of X", it can't infer it. So we have
+    // to annotate it, which sadly means we also have to annotate the 21 other type arguments...
     // :(
     #[rustfmt::skip]
     let transformed = transform::<_, _, _, _, _, _, _, Coprod!(Print, Lunchtime), _, _, _, _, _, _,
