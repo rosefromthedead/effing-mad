e2713313a7e85c9dc66a05bd0e1a1a9474b6c2bb
Rose Hudson <cv21874@bristol.ac.uk>
2022-09-08 11:59:16 +0100

    warn users of missing feature in sync-and-async
    closes #3

--- examples/sync-and-async.rs
+++ examples/sync-and-async.rs
@@ -1,62 +1,75 @@
 #![feature(generators)]
 #![feature(generator_trait)]
 
-use std::ops::ControlFlow;
-
-use effing_mad::{effectful, handle, handler, run, handle_async};
-
 fn main() {
-    let rt = tokio::runtime::Builder::new_current_thread()
-        .enable_all()
-        .build()
-        .unwrap();
-    rt.block_on(interesting_and_useful());
+    #[cfg(feature = "http")]
+    example::main();
 
-    boring_and_old_fashioned();
+    #[cfg(not(feature = "http"))]
+    eprintln!("this example requires the `http` feature to be enabled.");
+    #[cfg(not(feature = "http"))]
+    eprintln!("you can run it using `cargo run --example sync-and-async --features http`");
 }
 
-// very complex and powerful API
-effing_mad::effects! {
-    http::HttpRequest {
-        fn get(url: &'static str) -> String;
+#[cfg(feature = "http")]
+mod example {
+    use std::ops::ControlFlow;
+
+    use effing_mad::{effectful, handle, handler, run, handle_async};
+
+    pub fn main() {
+        let rt = tokio::runtime::Builder::new_current_thread()
+            .enable_all()
+            .build()
+            .unwrap();
+        rt.block_on(interesting_and_useful());
+
+        boring_and_old_fashioned();
     }
-}
-use http::HttpRequest;
 
-// this function does not specify whether the request happens synchronously or asynchronously
-#[effectful(HttpRequest)]
-fn example() -> usize {
-    let body = yield HttpRequest::get("http://example.com");
-    body.len()
-}
+    // very complex and powerful API
+    effing_mad::effects! {
+        http::HttpRequest {
+            fn get(url: &'static str) -> String;
+        }
+    }
+    use http::HttpRequest;
 
-async fn interesting_and_useful() {
-    let handler = handler! {
-        async http::HttpRequest,
-        get(url) => {
-            let body = reqwest::get(url).await.unwrap().text().await.unwrap();
-            ControlFlow::Continue(body)
-        },
-    };
+    // this function does not specify whether the request happens synchronously or asynchronously
+    #[effectful(HttpRequest)]
+    fn example() -> usize {
+        let body = yield HttpRequest::get("http://example.com");
+        body.len()
+    }
 
-    let req1 = handle_async(example(), handler);
-    let req2 = handle_async(example(), handler);
+    async fn interesting_and_useful() {
+        let handler = handler! {
+            async http::HttpRequest,
+            get(url) => {
+                let body = reqwest::get(url).await.unwrap().text().await.unwrap();
+                ControlFlow::Continue(body)
+            },
+        };
 
-    // asyncified effectful functions can be composed in the same ways as traditional futures
-    let (res1, res2) = futures::future::join(req1, req2).await;
-    println!("asynchronously found {res1} and {res2} bytes");
-}
+        let req1 = handle_async(example(), handler);
+        let req2 = handle_async(example(), handler);
 
-fn boring_and_old_fashioned() {
-    let handler = handler! {
-        http::HttpRequest,
-        get(url) => {
-            let body = reqwest::blocking::get(url).unwrap().text().unwrap();
-            ControlFlow::Continue(body)
-        },
-    };
+        // asyncified effectful functions can be composed in the same ways as traditional futures
+        let (res1, res2) = futures::future::join(req1, req2).await;
+        println!("asynchronously found {res1} and {res2} bytes");
+    }
 
-    let req = handle(example(), handler);
-    let res = run(req);
-    println!("synchronously found {res} bytes");
+    fn boring_and_old_fashioned() {
+        let handler = handler! {
+            http::HttpRequest,
+            get(url) => {
+                let body = reqwest::blocking::get(url).unwrap().text().unwrap();
+                ControlFlow::Continue(body)
+            },
+        };
+
+        let req = handle(example(), handler);
+        let res = run(req);
+        println!("synchronously found {res} bytes");
+    }
 }
